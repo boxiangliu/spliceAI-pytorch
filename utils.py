63fbfdfa2fc2e4ebf4f0aaf6349795045c31d54f
Boxiang Liu <jollier.liu@gmail.com>
2021-11-23 18:07:22 -0800

    debug

--- utils.py
+++ utils.py
@@ -56,8 +56,8 @@ def create_datapoints(seq, strand, tx_start, tx_end, jn_start, jn_end):
     tx_start = int(tx_start)
     tx_end = int(tx_end) 
 
-    jn_start = map(lambda x: map(int, re.split(',', x)[:-1]), jn_start)
-    jn_end = map(lambda x: map(int, re.split(',', x)[:-1]), jn_end)
+    jn_start = list(map(lambda x: map(int, re.split(',', x)[:-1]), jn_start))
+    jn_end = list(map(lambda x: map(int, re.split(',', x)[:-1]), jn_end))
 
     if strand == '+':
 
