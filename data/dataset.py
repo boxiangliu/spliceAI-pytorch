a15fae36caa34b25b2c0b46d2092423415148c69
Boxiang Liu <jollier.liu@gmail.com>
2021-11-25 10:20:38 -0800

    modified:   data/dataset.py 	new file:   model/model.py

--- data/dataset.py
+++ data/dataset.py
@@ -1,5 +1,6 @@
 import torch
 import h5py
+from tqdm import tqdm
 
 
 class H5Dataset(torch.utils.data.Dataset):
@@ -14,11 +15,11 @@ class H5Dataset(torch.utils.data.Dataset):
         num_examples = 0
         idx_to_key = {}
 
-        for k in sorted(self.h5f.keys(), key=lambda x: int(x[1:])):
+        for k in tqdm(sorted(self.h5f.keys(), key=lambda x: int(x[1:]))):
             assert k.startswith("X") or k.startswith("Y")
 
             if k.startswith("X"):
-                for idx in range(h5f[k].shape[0]):
+                for idx in range(self.h5f[k].shape[0]):
                     idx_to_key[idx + num_examples] = (k, idx)
                 num_examples += self.h5f[k].shape[0]
 
@@ -29,10 +30,16 @@ class H5Dataset(torch.utils.data.Dataset):
         return self.num_examples
 
     def __getitem__(self, idx):
-        Xk, idx = self.idx_to_key[k]
+        Xk, idx = self.idx_to_key[idx]
         Yk = Xk.replace("X", "Y")
-        return self.h5f[Xk][idx], self.h5f[Yk][0,idx]
+        return self.h5f[Xk][idx], self.h5f[Yk][0, idx]
+
 
 def test_H5Dataset():
     h5_filename = "data/dataset_train_all.h5"
-    dataset = H5Dataset()
+    dataset = H5Dataset(h5_filename)
+
+    for k in dataset.idx_to_key:
+        print(dataset.idx_to_key[k])
+        if k > 500:
+            break
---
+++ model/model.py
@@ -0,0 +1,69 @@
+import numpy as np
+import torch
+import torch.utils.data as data
+import torch.nn.functional as F
+import torch.nn as nn
+
+L = 32
+# convolution window size in residual units
+W = np.asarray([11, 11, 11, 11, 11, 11, 11, 11,
+                21, 21, 21, 21, 41, 41, 41, 41])
+# atrous rate in residual units
+AR = np.asarray([1, 1, 1, 1, 4, 4, 4, 4,
+                 10, 10, 10, 10, 25, 25, 25, 25])
+
+
+class ResBlock(nn.Module):
+    def __init__(self, L, W, AR, pad=True):
+        super(ResBlock, self).__init__()
+        self.bn1 = nn.BatchNorm1d(L)
+        s = 1
+        # padding calculation: https://discuss.pytorch.org/t/how-to-keep-the-shape-of-input-and-output-same-when-dilation-conv/14338/2
+        if pad:
+            padding = int(1 / 2 * (1 - L + AR * (W - 1) - s + L * s))
+        else:
+            padding = 0
+        self.conv1 = nn.Conv1d(L, L, W, dilation=AR, padding=padding)
+        self.bn2 = nn.BatchNorm1d(L)
+        self.conv2 = nn.Conv1d(L, L, W, dilation=AR, padding=padding)
+
+    def forward(self, x):
+        out = self.bn1(x)
+        out = torch.relu(out)
+        out = self.conv1(out)
+        out = self.bn2(out)
+        out = torch.relu(out)
+        out = self.conv2(out)
+        out = out + x
+        return out
+
+
+class Pangolin(nn.Module):
+    def __init__(self, L, W, AR):
+        super(Pangolin, self).__init__()
+        self.n_chans = L
+        self.conv1 = nn.Conv1d(4, L, 1)
+        self.skip = nn.Conv1d(L, L, 1)
+        self.resblocks, self.convs = nn.ModuleList(), nn.ModuleList()
+        for i in range(len(W)):
+            self.resblocks.append(ResBlock(L, W[i], AR[i]))
+            if (((i + 1) % 4 == 0) or ((i + 1) == len(W))):
+                self.convs.append(nn.Conv1d(L, L, 1))
+        self.conv_last = nn.Conv1d(L, 3, 1)
+
+    def forward(self, x):
+        conv = self.conv1(x)
+        skip = self.skip(conv)
+        j = 0
+        for i in range(len(W)):
+            conv = self.resblocks[i](conv)
+            if (((i + 1) % 4 == 0) or ((i + 1) == len(W))):
+                dense = self.convs[j](conv)
+                j += 1
+                skip = skip + dense
+        CL = 2 * np.sum(AR * (W - 1))
+        skip = F.pad(skip, (-CL // 2, -CL // 2))
+        out = F.softmax(self.conv_last(skip), dim=1)
+        return out
+
+
